8f13a1c4ad06d1a1318ec438f80b8999078d56f9
Dario La Placa <dariolaplacauni@gmail.com>
2023-02-06 19:20:59 +0100

    Secondo Esercizio

---
+++ src/exercise-two/Main.java
@@ -0,0 +1,54 @@
+package javabasics._3;
+
+public class Main {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+
+    /**
+     * 1: print out your initials using System.out.print and then a char literal,
+     * i.e. 'a', 'b', 'c'. You will need multiple print statements
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        System.out.print('D');
+        System.out.print('L');
+        System.out.print('P');
+    }
+
+    /**
+     * 2: Print out your age as an int literal, i.e. 28, then print whether or not you've
+     * had lunch today as a boolean literal i.e. true, false, then print the price of
+     * your lunch as a double, i.e. 4.99
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        // Write your code here
+        System.out.println(25);
+        System.out.println(true);
+        System.out.println(4.99);
+    }
+
+    /**
+     * 3: Complete exercise 2, but store the values in a variable.
+     * And then print out the variable.
+     *
+     * i.e.
+     * char favouriteLetter = 'g';
+     * System.out.print("My favourite letter=");
+     * System.out.println(favouriteLetter)
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+        // Write your code here
+        int myAge = 25;
+        boolean hadLunch = true;
+        double lunchPrice = 4.99d;
+
+        System.out.println(myAge);
+        System.out.println(hadLunch);
+        System.out.println(lunchPrice);
+    }
+}
