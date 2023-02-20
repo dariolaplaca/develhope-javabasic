917fc75960685b52ffcac9ee2dd6d6618c0b34cf
Dario La Placa <dariolaplacauni@gmail.com>
2023-02-20 15:39:59 +0100

    Aggiunto esercizio 4 OOP, Scope, access modifiers, static, final

---
+++ src/it/oop/exercise_four/ExerciseLunch.java
@@ -0,0 +1,69 @@
+package it.oop.exercise_four;
+
+public class ExerciseLunch {
+    private Double price = null;
+    private String nameOfDayEaten = null;
+    private int lunchWeightInGrams;
+
+    private static final double MAX_PRICE = 5;
+
+    private int numberOfTimesWeHaveGotPrice = 0;
+
+    public ExerciseLunch(Double price, String nameOfDayEaten) {
+        this.price = price;
+        this.nameOfDayEaten = nameOfDayEaten;
+    }
+
+    public static void printMaxPrice() {
+        System.out.println(MAX_PRICE);
+    }
+
+    private void printLunch() {
+        System.out.println("Lunch{" +
+                "price=" + price +
+                ", nameOfDayEaten='" + nameOfDayEaten + '\'' +
+                ", lunchWeightInGrams='" + lunchWeightInGrams + '\'' +
+                '}');
+    }
+
+    public void setPrice(double price) {
+        if (price > MAX_PRICE) {
+            System.out.println("Cannot set greater than max price");
+        } else {
+            this.price = price;
+        }
+    }
+
+    public Double getPrice() {
+        numberOfTimesWeHaveGotPrice = numberOfTimesWeHaveGotPrice + 1;
+        return this.price;
+    }
+
+    public int getNumberOfTimesWeHaveGotPrice() {
+        return this.numberOfTimesWeHaveGotPrice;
+    }
+
+    public String getNameOfDayEaten() {
+        return nameOfDayEaten;
+    }
+
+    public int getLunchWeightInGrams() {
+        return lunchWeightInGrams;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setNameOfDayEaten(String nameOfDayEaten) {
+        this.nameOfDayEaten = nameOfDayEaten;
+    }
+
+    public void setLunchWeightInGrams(int lunchWeightInGrams) {
+        this.lunchWeightInGrams = lunchWeightInGrams;
+    }
+
+    private void setNumberOfTimesWeHaveGotPrice(int numberOfTimesWeHaveGotPrice) {
+        this.numberOfTimesWeHaveGotPrice = numberOfTimesWeHaveGotPrice;
+    }
+}
