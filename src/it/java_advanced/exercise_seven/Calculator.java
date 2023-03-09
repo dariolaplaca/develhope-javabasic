b24704948fdb13615f5c7c423a31696970b09730
Dario La Placa <dariolaplacauni@gmail.com>
2023-03-09 15:56:39 +0100

    Added JUNIT exercise (exercise_seven) to java advanced

--- src/it/java_advanced/exercise_seven/Calculator.java
+++ src/it/java_advanced/exercise_seven/Calculator.java
@@ -3,7 +3,7 @@ package it.java_advanced.exercise_seven;
 // You can change any code in this class if you think there is a bug!
 public class Calculator {
     public int add(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public int subtract(int a, int b) {
@@ -11,7 +11,7 @@ public class Calculator {
     }
 
     public int multiply(int a, int b) {
-        return a * a;
+        return a * b;
     }
 
     public int divide(int a, int b) {
@@ -19,7 +19,7 @@ public class Calculator {
     }
 
     public double power(double base, double exponent) {
-        return Math.pow(exponent, base);
+        return Math.pow(base, exponent);
     }
 }
 
--- src/it/java_advanced/exercise_seven/CalculatorTest.java
+++ src/it/java_advanced/exercise_seven/CalculatorTest.java
@@ -20,6 +20,16 @@ class CalculatorTest {
     @Test
     void exercise1() {
         System.out.println("\nExercise 1: ");
+        Calculator c = new Calculator();
+        Assertions.assertEquals(5, c.add(3, 2));
+        Assertions.assertEquals(-5, c.add(-3, -2));
+        Assertions.assertEquals(7, c.add(-10, 17));
+        Assertions.assertEquals(0, c.add(0, 0));
+
+        Assertions.assertEquals(1, c.subtract(3, 2));
+        Assertions.assertEquals(-1, c.subtract(-3, -2));
+        Assertions.assertEquals(-27, c.subtract(-10, 17));
+        Assertions.assertEquals(0, c.subtract(0, 0));
         // Your code here
     }
 
@@ -39,8 +49,18 @@ class CalculatorTest {
         System.out.println("\nExercise 2: ");
         // Your code here
 
-        Calculator calculator = new Calculator();
-        Assertions.assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0));
+        Calculator c = new Calculator();
+        Assertions.assertThrows(ArithmeticException.class, () -> c.divide(10, 0));
+        Assertions.assertEquals(6, c.multiply(3, 2));
+        Assertions.assertEquals(6, c.multiply(-3, -2));
+        Assertions.assertEquals(-170, c.multiply(-10, 17));
+        Assertions.assertEquals(0, c.multiply(0, 0));
+        Assertions.assertEquals(0, c.multiply(100, 0));
+
+        Assertions.assertEquals(3, c.divide(6, 2));
+        Assertions.assertEquals(3, c.divide(-6, -2));
+        Assertions.assertEquals(-17, c.divide(-170, 10));
+        Assertions.assertEquals(0, c.divide(0, 100));
     }
 
     /**
@@ -58,6 +78,16 @@ class CalculatorTest {
     @Test
     void exercise3() {
         System.out.println("\nExercise 3: ");
+        Calculator c = new Calculator();
+        Assertions.assertEquals(1, c.power(10, 0));
+        Assertions.assertEquals(10, c.power(10, 1));
+        Assertions.assertEquals(100, c.power(10, 2));
+        Assertions.assertEquals(0.01, c.power(10, -2));
+
+        Assertions.assertEquals(0, c.power(0, 2));
+        Assertions.assertEquals(1, c.power(0, 0));
+        Assertions.assertEquals(100, c.power(-10, 2));
+        Assertions.assertEquals(-1000, c.power(-10, 3));
         // Your code here
     }
 }
