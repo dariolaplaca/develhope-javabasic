fbea0b7673b36a611acdc78c1401075878a3d463
Dario La Placa <dariolaplacauni@gmail.com>
2023-03-14 17:42:22 +0100

    Added exercise 2 in java_advanced_2 Object.class, Method overriding, equals, toString

---
+++ src/it/java_advanced_2/exercise_three/Person.java
@@ -0,0 +1,47 @@
+package it.java_advanced_2.exercise_three;
+
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + "\n" + this.address + "\n" + this.age + " years old";
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() * age * this.address.hashCode();
+    }
+}
