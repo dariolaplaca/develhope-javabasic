51b120061469f1476f9a4fcc1f86ee6b7bd69749
Dario La Placa <dariolaplacauni@gmail.com>
2023-03-20 14:21:46 +0100

    Added Exercise - Java Streams to java_advanced_3 - Exercise Two

---
+++ src/main/java/it/java_advanced_3/exercise_two/Exercises.java
@@ -0,0 +1,89 @@
+package it.java_advanced_3.exercise_two;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Exercises {
+
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
+    }
+
+    private static List<Integer> ourNumbers = IntStream.range(1, 10).boxed().toList();
+
+    /**
+     * 1:
+     *
+     *
+     * Use filter to select only even numbers from the list 'ourNumbers'
+     *
+     * Use forEach to print the selected even numbers
+     *
+     * %
+     */
+    private static void exercise1() {
+        System.out.println("\nExercise 1: ");
+        ourNumbers.stream().filter(a -> a % 2 == 0).forEach(System.out::println);
+        // Your code here
+    }
+
+    /**
+     * 2:
+     *
+     *
+     * Use filter to select only odd numbers from the list 'ourNumbers'
+     *
+     * Use the toSet() method to collect the selected odd numbers in a Set
+     *
+     * Print the resulting Set
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2: ");
+
+        Set<Integer> oddSet = ourNumbers.stream().filter(a -> a % 2 != 0).collect(Collectors.toSet());
+        System.out.println(oddSet);
+        // Your code here
+    }
+
+    /**
+     * 3:
+     *
+     *
+     * Use map to convert the strings to uppercase
+     *
+     * Use the toSet() method to collect the resulting uppercase strings in a Set
+     *
+     * Print the resulting Set
+     *
+     * BONUS: do so without creating any variables!
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3: ");
+        List<String> alice = List.of("Alice", "Bob", "Charlie");
+        System.out.println(alice.stream().map(String::toUpperCase).collect(Collectors.toSet()));
+        // Your code here
+    }
+
+    /**
+     * 4:
+     *
+     *
+     * Use filter to select only even numbers from the list 'ourNumbers'
+     *
+     * Use map to multiply the even numbers by 2
+     *
+     * Use the toSet() method to collect the resulting numbers in a Set
+     *
+     * Print the resulting Set
+     */
+    private static void exercise4() {
+        System.out.println("\nExercise 4");
+        System.out.println(ourNumbers.stream().filter(a -> a % 2 == 0).map(a -> a * 2).collect(Collectors.toSet()));
+        //Your code here
+    }
+}
