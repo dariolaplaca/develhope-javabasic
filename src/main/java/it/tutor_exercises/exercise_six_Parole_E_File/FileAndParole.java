ab43fe881b18d1a42a0d8b1fdbf65097cb00a1cf
Dario La Placa <dariolaplacauni@gmail.com>
2023-04-02 19:45:38 +0200

    Added a occorrenzaParoleTesto to FileAndParole

--- src/main/java/it/tutor_exercises/exercise_six_Parole_E_File/FileAndParole.java
+++ src/main/java/it/tutor_exercises/exercise_six_Parole_E_File/FileAndParole.java
@@ -9,7 +9,7 @@ public class FileAndParole {
     public static void main(String[] args) throws IOException {
         Path file = Path.of("src/main/java/it/tutor_exercises/exercise_six_Parole_E_File/text_input");
         stampaParole(file);
-        System.out.println(occorrenzeParole(file));
+        System.out.println(occorrenzeParoleTesto(file));
     }
 
     public static void stampaParole(Path file) throws IOException {
@@ -34,4 +34,34 @@ public class FileAndParole {
         }
         return parole;
     }
+
+    public static HashMap<String, Integer> occorrenzeParoleTesto(Path file) throws IOException {
+        List<String> righe = Files.readAllLines(file);
+        HashMap<String, Integer> parole = new HashMap<>();
+        for(String riga : righe){
+            riga = riga.replaceAll("\\W+","").toLowerCase();
+            for(String parola : riga.split(" ")){
+                if(parole.containsKey(parola)){
+                    int quanteParolePresenti = parole.get(parola);
+                    parole.put(parola, quanteParolePresenti + 1 );
+                } else {
+                    parole.put(parola, 1);
+                }
+            }
+        }
+        return parole;
+    }
+
+    public static HashMap<String, ArrayList<String>> paroleInRima(Path file) throws IOException{
+        List<String> righe = Files.readAllLines(file);
+        HashMap<String, ArrayList<String>> rime = new HashMap<>();
+        for(String riga : righe){
+            riga = riga.replaceAll("\\W+","").toLowerCase();
+            for(String parola : riga.split(" ")){
+                System.out.println(parola);
+            }
+        }
+        return rime;
+    }
+
 }
